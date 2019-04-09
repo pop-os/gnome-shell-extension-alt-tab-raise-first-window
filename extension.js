dd081ee18fd186ca9ac23622990440230cefb132
Jeremy Soller <jeremy@system76.com>
2019-04-09 15:51:27 -0600

    Fix support for disco

--- extension.js
+++ extension.js
@@ -1,27 +1,24 @@
-/* -*- mode: js; js-basic-offset: 4; indent-tabs-mode: nil -*- */
-
 const AltTab = imports.ui.altTab;
 const Main = imports.ui.main;
+const SwitcherPopup = imports.ui.switcherPopup;
 
 let injections = {};
 
-
 function _finish(timestamp) {
-        let appIcon = this._items[this._selectedIndex];
-        if (this._currentWindow < 0)
-            Main.activateWindow(appIcon.cachedWindows[0], timestamp);
-        else
-            Main.activateWindow(appIcon.cachedWindows[this._currentWindow], timestamp);
+    let appIcon = this._items[this._selectedIndex];
+    if (this._currentWindow < 0)
+        Main.activateWindow(appIcon.cachedWindows[0], timestamp);
+    else
+        Main.activateWindow(appIcon.cachedWindows[this._currentWindow], timestamp);
 
-        this.parent();
+    SwitcherPopup.SwitcherPopup.prototype._finish.apply(this, [timestamp]);
 }
 
-function init(metadata) {
-}
+function init(metadata) {}
 
 function enable() {
    injections._finish = AltTab.AppSwitcherPopup.prototype._finish;
-   AltTab.AppSwitcherPopup.prototype._finish = AltTab.AppSwitcherPopup.wrapFunction('_finish', _finish);
+   AltTab.AppSwitcherPopup.prototype._finish = _finish;
 }
 
 function disable() {
